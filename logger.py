e04bfe7bd8bb2978980d5b8cd6f98986c3320aac
Vivek Anand <vivekanand12104@gmail.com>
2026-08-24 23:01:18 +0530

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,21 @@
+"""
+Logging and telemetry utilities for VisionWare AI.
+"""
+
+import logging
+import sys
+
+def setup_logger(name: str = "VisionWareAI") -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter(
+            fmt="%(asctime)s [%(levelname)s] [%(name)s] %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
+
+logger = setup_logger()
